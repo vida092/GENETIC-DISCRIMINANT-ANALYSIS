b4157752629c851d01d7f0d33f84b9183ec22eb1
Victor David Garcia Medina <31834801+vida092@users.noreply.github.com>
2025-04-12 17:47:51 -0600

    Add files via upload

--- Main3D.m
+++ Main3D.m
@@ -1,13 +1,13 @@
 clc
 clear
-database_list = {"diabetes", "datos8","wine","colon_encoded"};
+database_list = {"iris", "golub_cleaned"};
 
 
 for i = 1:numel(database_list)
     database = database_list{i};
     tic
-    disp("procesando base de datos " + database + " caso 2D")
-    genetic_algorithm_2D(database)
+    % disp("procesando base de datos " + database + " caso 2D")
+    % genetic_algorithm_2D(database)
     
     disp("procesando base de datos " + database + " caso 3D")
     genetic_algorithm_3D(database)   
